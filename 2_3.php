e8d8fb180abd7cc9a691f4a8fbccff6ec085fbf6
oleg <oleg.ovsyannikov.96@gmail.com>
2023-02-15 16:42:15 +0300

    Глава 2 Задача 3

---
+++ 2_3.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * В массиве А(N) подсчитать количество различных элементов.
+ */
+function getAmountNumberOfDifferent ($arr) {
+    $total = count($arr);
+    for ($i = 0; $i < count($arr); $i++) {
+        for ($j = $i + 1; $j < count($arr); $j++) {
+            if ($arr[$i] === $arr[$j]) {
+                $total--;
+            }
+        }
+    }
+}
+
+echo getAmountNumberOfDifferent([2, 4, 5, 6, 3, 2, 7]);
