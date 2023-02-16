b3d51ec22da65a03f9a13ce306448929c550574c
oleg <oleg.ovsyannikov.96@gmail.com>
2023-02-16 11:49:12 +0300

    Глава 2 Задача 6

--- 2_6.php
+++ 2_6.php
@@ -3,3 +3,19 @@
  * Получить упорядоченный по возрастанию массив С(К) путем слияния двух порядоченных 
  * по возрастанию массивов А(N) и В(М), где К=М+N.
  */
+$arr1 = [1, 3, 5, 6, 9];
+$arr2 = [0, 1, 2, 4, 5, 7, 8, 9];
+$res = [];
+
+$lArr = count($arr1) < count($arr2) ? $arr1 : $arr2;
+$bArr = count($arr1) > count($arr2) ? $arr1 : $arr2;
+
+for ($i = 0; $i < count($lArr); $i++) {
+    $res[] = $lArr[$i] + $bArr[$i];
+}
+
+for ($j = count($lArr); $j < count($bArr); $j++) {
+    $res[] = $bArr[$j];
+}
+
+print_r($res);
